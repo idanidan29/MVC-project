1b0db2ac756439945c3e2d1f4e8f39ba7567cd32
zechariah315 <zechariah151299@gmail.com>
2026-01-02 15:29:57 +0200

    color of inputs

--- obj/Debug/net8.0/MVC-project.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC-project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d8e838f76155c78e203d2360df121a86e5b844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c479242d6541e6ad543bdfdbb2f92e26a14911")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
